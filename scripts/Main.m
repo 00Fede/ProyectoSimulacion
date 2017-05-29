4a2f00205cb6434d5e1dbb40c3c1a8755b056693
00Fede <feedkiko@gmail.com>
2017-05-28 23:54:20 -0500

    Implementacion vecinos cercanos, calculo curva ROC

--- scripts/Main.m
+++ scripts/Main.m
@@ -18,6 +18,70 @@ switch(punto)
         % Funciones Discriminantes Gaussianas
         X = X(:,[1:14,18,19]); % quitamos cols cuya var es 0
         eficiencia = zeros(1,rept); %vector fila para guardar eficiencia de c/fold
+        AUC = zeros(1,rept);
+        rng('default');
+        particion = cvpartition(N,'k',rept);  %genera validación cruzada
+        % Crea e inicializa un objeto de performance de clasificador, Y son
+        % los truelabels de cada una de las observaciones. Permite mantener
+        % un completo escaneo del performance del clasificador
+        CP = classperf(Y); 
+        for fold=1:rept
+            
+            Xtrain=X(particion.training(fold),:);
+            Ytrain=Y(particion.training(fold));
+            Xtest=X(particion.test(fold),:);
+            Ytest=Y(particion.test(fold));
+            
+            % Genera el modelo con las muestra de entrenamiento
+            model = fitcdiscr(Xtrain,Ytrain,'DiscrimType','diagquadratic'); 
+            % hace prediccion de muestras de validacion. El score es usado
+            % para graficar la curva ROC
+            [Yesti,score] = model.predict(Xtest);
+            % Como interesa scores de clase 1, se divide probabilidad de
+            % clase 1 sobre probabilidad de clase 0
+            score = score(:,1)./score(:,2);
+            % Actualiza el objeto de performance del clasificador con la
+            % salida del clasificador Yesti, el ultimo argumento indica las
+            % observaciones que fueron usados en la validacion actual
+            classperf(CP,Yesti,particion.test(fold));
+            % Necesaria unicamente para obtener Intervalo de Confianza
+            eficiencia(fold) = sum(Yesti==Ytest)/length(Ytest);
+            % Calculo de curva ROC
+            [Xroc,Yroc,~,AUC(fold)] = perfcurve(Ytest,score,'0');
+            plot(Xroc,Yroc);
+            title('Curva ROC');
+            xlabel('1 - Especificidad');
+            ylabel('Sensibilidad');
+            hold all;
+        end
+        % TODO: 
+        Eficiencia = get(CP,'CorrectRate');
+        Sensibilidad = get(CP,'Sensitivity');
+        Especificidad = get(CP,'Specificity');
+        Precision = get(CP,'PositivePredictiveValue'); %Precision
+        IC = std(eficiencia);
+        AUC = mean(AUC);
+        ICAUC = std(AUC);
+        
+        Text = ['La eficiencia obtenida de funciones discr. gauss. es ',num2str(Eficiencia),' +-',num2str(IC)];
+        disp(Text);
+        Text = ['Sensibilidad: ',num2str(Sensibilidad)];
+        disp(Text);
+        Text = ['Especificidad: ',num2str(Especificidad)];
+        disp(Text);
+        Text = ['Precision: ',num2str(Precision)];
+        disp(Text);
+         % Escribo el desempe�o esperado del sistema
+        Texto=['desempeno esperado del sistema (AUC) es = ', num2str(AUC),' +- ',num2str(ICAUC)];
+        disp(Texto);
+        %%% Fin Funciones discriminantes gaussianas %%%
+        
+    case 2
+        % K vecinos
+        X = X(:,[1:14,18,19]); % quitamos cols cuya var es 0
+        k = 5;
+        eficiencia = zeros(1,rept); %vector fila para guardar eficiencia de c/fold
+        ecm = zeros(1,rept);
         rng('default');
         particion = cvpartition(N,'k',rept);  %genera validación cruzada
         % Crea e inicializa un objeto de performance de clasificador, Y son
@@ -30,26 +94,32 @@ switch(punto)
             Xtest=X(particion.test(fold),:);
             Ytest=Y(particion.test(fold));
             
-            % Genera el modelo con las muestra de entrenamiento
-            model = fitcdiscr(Xtrain,Ytrain,'DiscrimType','diaglinear'); 
-            % hace prediccion de muestras de validacion
-            Yesti = model.predict(Xtest);
-            % Actualiza el objeto de performance del clasificador con la
-            % salida del clasificador Yesti, el ultimo argumento indica las
-            % observaciones que fueron usados en la validacion actual
-            classperf(CP,Yesti,particion.test(fold));
-            % Necesaria unicamente para obtener Intervalo de Confianza
-            eficiencia(fold) = sum(Yesti==Ytest)/length(Ytest);
+            %%% Normalizaci�n %%%
+            [Xtrain,mu,sigma]=zscore(Xtrain);
+            Xtest=normalizar(Xtest,mu,sigma);
             
+            % Aplica modelo k-nearest a datos de entrenamiento y genera un
+            % Yesti en base a Xtest
+            Yesti = vecinosCercanos(Xtest,Xtrain,Ytrain,k);
+            
+            classperf(CP,Yesti,particion.test(fold));
+            eficiencia(fold)=sum(Yesti==Ytest)/length(Ytest);
+            % Se calcula Error Cuadratico Medio de Yesti y Ytest
+            ecm(fold) = immse(Yesti,Ytest);
+
         end
-        % TODO: 
+        ecm = mean(ecm); % Promedio del error cuadratico medio
         Eficiencia = get(CP,'CorrectRate');
+        Error = get(CP,'ErrorRate');
         Sensibilidad = get(CP,'Sensitivity');
         Especificidad = get(CP,'Specificity');
         Precision = get(CP,'PositivePredictiveValue'); %Precision
         IC = std(eficiencia);
+        IC1 = std(ecm);
         
-        Text = ['La eficiencia obtenida de funciones discr. gauss. es ',num2str(Eficiencia),' +-',num2str(IC)];
+        Text = ['El ECM obtenido de K-nearest es ',num2str(ecm),' +-',num2str(IC1)];
+        disp(Text);
+        Text = ['La eficiencia obtenida de K-nearest es ',num2str(Eficiencia),' +-',num2str(IC)];
         disp(Text);
         Text = ['Sensibilidad: ',num2str(Sensibilidad)];
         disp(Text);
@@ -57,14 +127,10 @@ switch(punto)
         disp(Text);
         Text = ['Precision: ',num2str(Precision)];
         disp(Text);
-        %%% Fin Funciones discriminantes gaussianas %%%
+        Text = ['Tasa de Error: ',num2str(Error)];
+        disp(Text);
         
-    case 2
-        % K vecinos
-        
-        
-        
-        
+        %%% Fin de K-nearest %%%
     case 3
         % RNA
     case 4
--- scripts/vecinosCercanos.m
+++ scripts/vecinosCercanos.m
@@ -1,10 +1,8 @@
-function Yesti = vecinosCercanos(Xval,Xent,Yent,k,tipo)
-
-    %%% El parametro 'tipo' es el tipo de problema que se va a resolver
+function Yesti = vecinosCercanos(Xval,Xent,Yent,k)
     
-    %%% La función debe retornar el valor de predicción Yesti para cada una de 
-    %%% las muestras en Xval. Por esa razón Yesti se inicializa como un vectores 
-    %%% de ceros, de dimensión M.
+    %%% La funciï¿½n debe retornar el valor de predicciï¿½n Yesti para cada una de 
+    %%% las muestras en Xval. Por esa razï¿½n Yesti se inicializa como un vectores 
+    %%% de ceros, de dimensiï¿½n M.
 
     N=size(Xent,1); % N = Numero de muestras de entrenamiento
     M=size(Xval,1); % M = Numero de muestras para la validacion
@@ -12,18 +10,18 @@ function Yesti = vecinosCercanos(Xval,Xent,Yent,k,tipo)
     Yesti=zeros(M,1);
     dis=zeros(N,M);  % Primero tenia (N,1) dimensiones y mejor le puse (N,M) dimensiones
     
-    % >>>>>>>>> 2º Aqui se calcula las distancias para saber la separacion de todo con todos
+    % >>>>>>>>> 2ï¿½ Aqui se calcula las distancias para saber la separacion de todo con todos
     dis = repmat(sum(Xent.^2,2),1,M) + repmat(sum(Xval.^2,2)',N,1) - 2*(Xent*Xval');
     dis = sqrt(dis);
     % Para entender plenamente como calculo todas las distancias con solo
     % estas pocas instrucciones vease el archivo: estrategiakvecinos[Distancias].pdf
     % Alli desgloso cada una de las partes de estas instrucciones.
     
-     % >>>>>>>>> 3º Determino los vecinos
+     % >>>>>>>>> 3ï¿½ Determino los vecinos
      
      % ============ Nota ============
      % Transpongo la matriz dis antes de ordenarla porque
-     % así en cada fila me quedaran las distancias de cada
+     % asï¿½ en cada fila me quedaran las distancias de cada
      % muestra  de validacion con respecto a las muestras
      % de cada una de las muestras de entrenamiento.
      % Por ejemplo:
@@ -36,7 +34,7 @@ function Yesti = vecinosCercanos(Xval,Xent,Yent,k,tipo)
      % muestra de validacion 1 con respecto a la muestra
      % de entrenamiento 2.
      % 
-     % Y así sucesivamente.
+     % Y asï¿½ sucesivamente.
      %
      % Esto es necesario porque a final de cuentas yo tengo es que entregar
      % la prediccion en base a mis muestras de validacion.
@@ -52,12 +50,12 @@ function Yesti = vecinosCercanos(Xval,Xent,Yent,k,tipo)
      [~, indices] = sort(dis', 2); 
      % Ya con una matriz de indices que esta ordenada de menor a mayor
      % de acuerdo al valor, entonces tomare todas las filas de dicha matriz
-     % pero solo las primera K columnas. Donde K seran los K más
+     % pero solo las primera K columnas. Donde K seran los K mï¿½s
      % cercanos para cada muestra de validacion.
      indices = indices(:, 1:k);
      
      
-    % >>>>>>>>> 4º Por ultimo, calculo el Y*
+    % >>>>>>>>> 4ï¿½ Por ultimo, calculo el Y*
     % ============ Nota ============
     % Los ciclos no los necesito porque como calculo de todo y por
     % todas las distancias entonces al final no requiero iterar
@@ -82,24 +80,6 @@ function Yesti = vecinosCercanos(Xval,Xent,Yent,k,tipo)
     %
     % Estoy seguro que si las observa con atencion se dara cuanta de como
     % se seleccionan los datos
-    if strcmp(tipo,'class') % Para clasificación
+   Yesti = mode(Yent(indices), 2); % Aqui se calcula la moda por filas y se retorna como Yestimada
 
-        %for j=1:M 
-            %%% Complete el codigo %%%
-            
-            Yesti = mode(Yent(indices), 2); % Aqui se calcula la moda por filas y se retorna como Yestimada
-            
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%
-            
-        %end
-    elseif strcmp(tipo,'regress') % Para regresion
-        
-        %for j=1:M
-            %%% Complete el codigo %%%
-            
-            Yesti = mean(Yent(indices), 2); % Aqui se calcula la media por filas y se retorna como Yestimada
-            
-			%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %end
-    end
 end
